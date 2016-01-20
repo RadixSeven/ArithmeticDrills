6c1bf88449cfb4daa0b03f27d0dbcb91f0aeee78
Eric Moyer <eric_moyer@yahoo.com>
2016-01-19 22:33:08 -0500

    Add html output

--- generate_tests.py
+++ generate_tests.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import random
 
 add_min = 0
 add_max = 10
@@ -15,6 +16,9 @@ class Fact:
         return "{} {} {} =".format(str(self.first), str(self.operator),
                                    str(self.second))
 
+
+# Generate the facts
+
 facts = set()
 
 for first in range(add_min, add_max+1):
@@ -24,6 +28,90 @@ for first in range(add_min, add_max+1):
         facts.add(Fact(s,"-",first))
         facts.add(Fact(s,"-",second))
 
+
+# Shuffle them
+facts = list(facts)
+random.shuffle(facts)
+
+
+# Create HTML
+
+# Headers and footers
+doc_header=r"""
+<html>
+<head>
+<style>
+@media all {
+	.page-break	{ display: none; }
+}
+
+@media print {
+	.page-break	{ display: block; page-break-before: always; }
+}
+
+table {
+    width: 100%;
+}
+
+td {
+    font-size: 0.345in;
+}
+
+</style>
+<body>
+"""
+doc_footer=r"""
+</body>
+</html>
+"""
+table_head="""
+<table>
+"""
+table_foot="""
+</table>
+<div class="page-break"></div>
+"""
+last_table_foot="""
+</table>
+"""
+row_head="""<tr>"""
+row_foot="""</tr>
+"""
+
+# Print tables
+print(doc_header)
+
+tables_per_printing = 2
+facts_per_row = 4
+rows_per_table = 20
+facts_per_table = facts_per_row * rows_per_table
+
+num_tables = 0
+num_in_current_table = 0
+num_in_current_row = 0
 for f in facts:
-    print(str(f))
-    print(repr(f))
+    if num_in_current_table == 0:
+        print(table_head)
+    if num_in_current_row == 0:
+        print(row_head)
+    print("<td>{}</td>".format(str(f)))
+    num_in_current_table += 1
+    num_in_current_row += 1
+    if num_in_current_row == facts_per_row:
+        print(row_foot)
+        num_in_current_row = 0
+    if num_in_current_table == facts_per_table:
+        num_in_current_table = 0
+        num_tables += 1
+        if num_tables == tables_per_printing:
+            print(last_table_foot)
+            break
+        else:
+            print(table_foot)
+
+if num_in_current_row > 0:
+    print(row_foot)
+if num_in_current_table > 0:
+    print(last_table_foot)
+
+print(doc_footer)
